ca6be1d89ba215fed98cf93cc88d82a39f0210e5
ipsjamesortiza <james.ortiz.aldana@gmail.com>
2021-06-27 12:48:36 -0500

    Add project files.

---
+++ WebApiFileUpload/Services/IStorageService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+
+namespace WebApiFileUpload.Services
+{
+    public interface IStorageService
+    {
+        void Upload(IFormFile formFile);
+    }
+}
